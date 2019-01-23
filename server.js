e0d2ac7a2ea636ec516609e33ec4c0c395208261
dair68 <granthuang68@yahoo.com>
2019-01-22 18:16:40 -0800

    can add articles to mongodb

--- server.js
+++ server.js
@@ -7,17 +7,64 @@ const cheerio = require("cheerio");
 const app = express();
 const PORT = 3000;
 
-app.get("/", function(req, res) {
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    note: Schema.Types.ObjectId,
+    saved: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const NoteSchema = new Schema({
+    title: {
+        type: String,
+        default: ""
+    },
+    body: {
+        type: String,
+        default: ""
+    }
+});
+
+const ArticlesDB = mongoose.model("Articles", ArticleSchema);
+
+//obtains all the articles currently in mongo db
+app.get("/", function (req, res) {
+    ArticlesDB.find({}, function (err, docs) {
+        if (err) {
+            throw err;
+        }
+
+        res.json(docs);
+        //console.log(docs);
+    });
+});
+
+//scrapes the huffington post and adds articles to mongo db
+app.post("/scrape", function (req, res) {
     res.send("Hello World");
     axios.get('https://www.huffingtonpost.com')
-        .then(response => {
+        .then(function (response) {
             if (response.status === 200) {
                 const html = response.data;
                 const $ = cheerio.load(html);
                 //console.log(html);
 
                 let headlines = [];
-                $(".card__headline").each(function(i,elem) {
+                $(".card__headline").each(function (i, elem) {
                     const title = $(this).find($(".card__headline__text")).text().trim();
                     const link = $(this).find($(".card__link")).attr("href");
 
@@ -27,26 +74,14 @@ app.get("/", function(req, res) {
                     });
                 });
 
-                console.log(headlines);
+                //console.log(headlines);
+
+                ArticlesDB.insertMany(headlines, err => { if (err) throw err; });
+                res.send("scrape complete");
             }
         }).catch(error => console.log(error));
 });
 
 app.listen(PORT, () => console.log("App listening on port " + PORT + "!"));
 
-// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-// mongoose.connect(MONGODB_URI);
 
-// const Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
-
-// const ArticleSchema = new Schema({
-//     title     : String,
-//     link      : String,
-//     note      : Schema.Types.ObjectId,
-//     saved     : Boolean
-// });
-
-// const NoteSchema = new Schema({
-//     title     : String,
-//     body      : String
-// })
